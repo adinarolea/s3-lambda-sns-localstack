4a0d511fd03b5cb88f0be362781afbb62729647b
adina <roleaa.adina@gmail.com>
2020-09-16 20:32:29 +0300

    added initial s3 & lambda function

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # s3-lambda-sns-localstack
 An example on how to use localstack to simulate a real AWS environment. The project will detect the bucket change, trigger a lambda and lambda will publish in a sns topic
+
+Check which services are running
+
+``
+curl localhost:4566/health
+``
+
+Create s3 bucket
+
+``
+  aws --endpoint=http://localhost:4566 s3 ls
+  aws --endpoint=http://localhost:4566 s3api create-bucket --bucket cool-camel --region us-east-1
+
+``
+
+Create lambda function
+``./mvnw  clean package shade:shade``
+
+List lambda functions
+
+``
+   aws --endpoint=http://localhost:4566 lambda list-functions
+``
+
+Create lambda function in localstack
+
+``
+ aws --endpoint=http://localhost:4566 lambda create-function --function-name s3_change_handler \
+ --zip-file fileb://s3-lambda-function/target/s3-lambda-function-1.0-SNAPSHOT.jar \
+ --handler com.tutorial.ar.lambda.S3ChangeLambdaFunction --runtime java8 --role arn:aws:iam::123456789012:role/lambda-cli-role
+ ``
+ 
+ Update bucket configuration to send notifications
+ 
+ ``
+ aws --endpoint=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cool-camel \
+  --notification-configuration file://config/notification.json
+``
+
+Add a file to the bucket
+
+ ``
+ aws --endpoint=http://localhost:4566 s3 cp config/camel.json s3://cool-camel/camel1.json
+``
---
+++ s3-lambda-function/src/main/java/com/tutorial/ar/lambda/S3ChangeLambdaFunction.java
@@ -0,0 +1,38 @@
+package com.tutorial.ar.lambda;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.CreateTopicResult;
+
+public class S3ChangeLambdaFunction implements RequestHandler<S3Event, String> {
+
+    AmazonSNS amazonSNS = AmazonSNSClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("mock", "mock")))
+            .withEndpointConfiguration(
+                    new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-east-1"))
+            .build();
+
+
+    @Override
+    public String handleRequest(S3Event s3Event, Context ctx) {
+        // For each record.
+        for (S3EventNotification.S3EventNotificationRecord record : s3Event.getRecords()) {
+
+            String s3Key = record.getS3().getObject().getKey();
+            String s3Bucket = record.getS3().getBucket().getName();
+
+            //if the topic exists, the aws will return it in this call
+            CreateTopicResult createTopicResult = amazonSNS.createTopic(s3Bucket);
+            amazonSNS.publish(createTopicResult.getTopicArn(), s3Key, s3Bucket);
+        }
+        return "Done";
+    }
+}
